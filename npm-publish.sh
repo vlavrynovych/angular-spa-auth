0b253b2caa9619e62c875c5ce6e4c68976922209
Volodymyr Lavrynovych <volodya.lavrynovych@gmail.com>
2017-08-10 17:35:31 +0300

    Update min version

---
+++ npm-publish.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+read -p "Are you sure you want to publish (Y/N) $1? " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    echo "Publishing $1..."
+
+    gulp build
+    npm test
+    git add . && git commit -a -m "Update min version"
+#    npm version
+    #update bower.json and package.json versions
+    #add tag
+    #commit and push
+
+#    npm publish ./
+fi
